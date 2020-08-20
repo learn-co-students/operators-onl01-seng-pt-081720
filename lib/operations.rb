e5297cfdca3b108b00374116d667496debfc8a35
Jeremy Wood <js.wood206@gmail.com>
2020-08-20 04:05:57 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	elsif speed >= 40 && speed <= 60
+		return false
+	else
+		return "Incorrect parameter"
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	 speed < 40 || speed > 60 ? true : false
 end
-	
-
-
