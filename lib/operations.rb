9367579509e388d408ede44b910c48d63b807bf6
Christian Pardo <chrispardopersonal@gmail.com>
2020-08-20 14:59:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
-
-end
+if speed > 60 || speed < 40
+  return true 
+else speed > 40 && speed < 60 
+  return false 
+  end
+end 
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60 || speed < 40 
+	  return true 
+	else speed > 40 && speed < 60 
+  return false 
+  end
 end
 	
 
