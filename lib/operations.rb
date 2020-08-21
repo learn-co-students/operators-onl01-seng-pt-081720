f1d66f25b939234b40872c0003e8eb4dcef87a64
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-21 23:38:22 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,18 @@
-def unsafe?(speed)
+
+def unsafe?(speedle)
+  
+  speedle == 50 ? 40 < 40 : 60 < 90
+  
 
 end
 
 
 
 def not_safe?(speed)
+  
+  speed == 50 ?  40 < 40 : 60 < 90
+  
+    
 	
 end
 	
