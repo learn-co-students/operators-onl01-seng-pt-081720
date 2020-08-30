7ce0b4ace091a7bfb53659a413ae570854c6981f
Myles Janik <mylesjanik@gmail.com>
2020-08-30 01:04:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,14 @@
 def unsafe?(speed)
   if (speed < 40 || speed > 60)
-    return true
+    return TRUE
   else
-    return false
+    return FALSE
 end
 
 
 
 def not_safe?(speed)
-  speed < 40 || speed > 60 ? return true : return false
-	
+  speed < 40 || speed > 60 ? return TRUE : return FALSE
 end
 	
 
